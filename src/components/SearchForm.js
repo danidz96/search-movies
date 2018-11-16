c9d93ea1c0fc4028495d9c7ecc846327e960e37d
danidz96 <37420305+danidz96@users.noreply.github.com>
2018-11-16 13:52:31 +0100

    controlar los eventos onChange y onSubmit cambiando los valores del estado y consumiendo la API

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react'
 
+const API_KEY = "9a5934eb"
+
 export default class SearchForm extends Component {
+  state = {
+    inputText: ''
+  }
+
+  _handleChange = (e) => {
+    this.setState({
+      inputText: e.target.value
+    })
+  }
+
+  _handleSubmit = (e) => {
+    e.preventDefault()
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${this.state.inputText}`)
+    .then(res => res.json())
+    .then(data => {
+      const { Search, totalResults } = data
+    })
+  }
+
   render() {
     return (
-      <div>
+      <form onSubmit={this._handleSubmit}>
         <div className="field has-addons">
           <div className="control">
-            <input className="input" type="text" placeholder="Find a repository" />
+            <input 
+              className="input"
+              onChange={this._handleChange} 
+              type="text" 
+              placeholder="Movie to search..." />
           </div>
           <div className="control">
-            <a className="button is-info">Search</a>
+            <button className="button is-info">Search</button>
           </div>
         </div>
-      </div>
+      </form>
     )
   }
 }
