6e9a83f687cb92209898168cdc0089e194016fa6
danidz96 <37420305+danidz96@users.noreply.github.com>
2018-11-16 14:31:12 +0100

    añadimos estado y le pasamos los resultados desde el SearchForm

--- src/App.js
+++ src/App.js
@@ -5,11 +5,22 @@ import { Title } from './components/Title'
 import SearchForm from './components/SearchForm';
 
 class App extends Component {
+  state = {
+    results: []
+  }
+
+  _handleResults = (results) => {
+    this.setState({ results })
+  }
+
   render() {
     return (
       <div className="App">
         <Title>Search Movies</Title>
-        <SearchForm />
+        <div className="SearchForm-wrapper">
+          <SearchForm onResults={this._handleResults}/>
+        </div>
+        {this.state.results.length === 0 ? <p>Sin resultados</p> : <p>Con resultados</p>}
       </div>
     );
   }
